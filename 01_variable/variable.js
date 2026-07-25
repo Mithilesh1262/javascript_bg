296c28c3e92e35a8d6079bb16a5cf303ce9b5dff
MITHILESH YADAV <mithilesh.ya01@gmail.com>
2026-07-25 05:43:20 +0000

    connfig done

---
+++ 01_variable/variable.js
@@ -0,0 +1,14 @@
+const amountId = 100;
+
+let authorEmail = "mithilesh@gmail.com";
+
+
+var amountPassword = "mithilesh@123";
+
+
+amount = 200;
+
+console.log(amountId);
+console.log(authorEmail);
+console.log(amountPassword);
+console.log(amount);
